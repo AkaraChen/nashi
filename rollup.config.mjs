a5aadf49d895fb1f93bc433941886728ea0a27dc
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-11-11 10:44:56 +0800

    ✨ feat: es5 build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,29 @@
 import esbuild from 'rollup-plugin-esbuild';
 import copy from 'rollup-plugin-copy';
 import filesize from 'rollup-plugin-filesize';
+import babel from '@rollup/plugin-babel';
 
-export default {
-    input: './src/index.js',
-    output: {
-        dir: 'dist',
+export default [
+    {
+        input: './src/index.js',
+        output: {
+            dir: 'dist',
+        },
+        plugins: [
+            esbuild({ minify: true }),
+            copy({ targets: [{ src: './src/index.d.ts', dest: './dist' }] }),
+            filesize(),
+        ],
     },
-    plugins: [
-        esbuild({ minify: true }),
-        copy({ targets: [{ src: './src/index.d.ts', dest: './dist' }] }),
-        filesize(),
-    ],
-};
+    {
+        input: './src/index.js',
+        output: {
+            file: 'dist/legacy.js',
+        },
+        plugins: [
+            esbuild({ minify: true }),
+            filesize(),
+            babel({ babelHelpers: 'bundled' }),
+        ],
+    },
+];
